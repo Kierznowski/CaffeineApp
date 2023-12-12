93eaa4891878a96bd7225273b68a3899110ce748
Kierznowski <przemek.kierznowski@gmail.com>
2023-12-12 10:26:52 +0100

    CoffeineApp adding repository
    Web application for  ordering custom coffees. It's app for Spring learning purpose.
    Last changes include implementation of JPA based repositories.

---
+++ app/src/main/java/com/Caffeine/app/configuration/WebConfig.java
@@ -0,0 +1,18 @@
+package com.Caffeine.app.configuration;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewResolverRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+import org.thymeleaf.spring6.SpringTemplateEngine;
+import org.thymeleaf.spring6.view.ThymeleafViewResolver;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addViewControllers(ViewControllerRegistry registry) {
+        registry.addViewController("/").setViewName("home");
+    }
+
+}
