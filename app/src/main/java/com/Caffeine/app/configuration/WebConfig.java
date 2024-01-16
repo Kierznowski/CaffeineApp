61ac55e896a2b710e8bc433f405a2cf8af358cae
Kierznowski <przemek.kierznowski@gmail.com>
2024-01-16 15:53:04 +0100

    front-end changes

--- app/src/main/java/com/Caffeine/app/configuration/WebConfig.java
+++ app/src/main/java/com/Caffeine/app/configuration/WebConfig.java
@@ -12,6 +12,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addViewControllers(ViewControllerRegistry registry) {
         registry.addViewController("/").setViewName("home");
         registry.addViewController("/login").setViewName("login");
+        registry.addViewController("/contact").setViewName("contact");
     }
 
 
