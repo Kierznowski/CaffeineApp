e02dd232ba94044be4fcc8accbfd5ffbc30a2428
Kierznowski <przemek.kierznowski@gmail.com>
2024-06-03 08:03:16 +0200

    Unit Tests
    Adding Unit tests

---
+++ app/src/test/java/com/Caffeine/app/controllers/OrderControllerTest.java
@@ -0,0 +1,31 @@
+package com.Caffeine.app.controllers;
+
+import com.Caffeine.app.model.CoffeeOrder;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.test.context.support.WithMockUser;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+public class OrderControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    @WithMockUser(username="test", roles={"USER"})
+    void shouldReturnOrderPage() throws Exception{
+
+        CoffeeOrder mockOrder = new CoffeeOrder();
+
+        this.mockMvc.perform(get("/orders/current").flashAttr("coffeeOrder", mockOrder))
+                .andExpect(status().isOk())
+                .andExpect(view().name("orderForm"));
+    }
+}
